fa653e40c2f054f8076ff2d992154a2d64ccac64
GaganjotSidhu <sherrynijjar1@gmail.com>
2014-06-10 23:31:00 -0700

    Update alldata.py
    Scraping educational background and industry along with previous titles. Writing data to a JSON file.

--- alldata.py
+++ alldata.py
@@ -1,35 +1,56 @@
 #!/usr/bin/python
 
+!/usr/bin/env python
+import json
 import urllib2
 import nltk
 from bs4 import BeautifulSoup
 import csv
-def main():
-	pasdatad = {}
-	profiles = ['daladamson','anmolnijjar', 'satchipanda']
-	for profile_name in profiles:
-		url = "https://www.linkedin.com/in/" + profile_name
-		page = urllib2.urlopen(url)
-		soup = BeautifulSoup(page)
-		title_span = soup.find_all('span', class_="title")
-		titles = []
-		for row in title_span:
-			titles.append(list(row.stripped_strings))
-		pasdatad[profile_name] = titles
-		#print pasdatad[profile_name]
-		print profile_name
-		print pasdatad[profile_name]
+import itertools
+finalData = {}
+pasdatad = {}
+profiles = []
+urls = []
+degree = []
+industry = []
+
+profile = "/Users/gagan/dev/linkedin-graph/profilenames.csv"
+profileReader = csv.reader(open(profile, 'rU'), delimiter=' ', quotechar='|')
+for row in profileReader:   
+	profiles.append(','.join(row))
+title = "/Users/gagan/dev/linkedin-graph/urls.csv"
+titleReader = csv.reader(open(title, 'rU'), delimiter=' ',  quotechar='|')
+for titl in titleReader:
+    urls.append(','.join(titl))
+for url, profile_name in zip(urls, profiles):
+	page = urllib2.urlopen(url)
+	soup = BeautifulSoup(page)
+	title_span = soup.find_all('span', class_="title")
+	degree_span = soup.find_all('span', { 'class' : ["degree", "major"]})
+	#major_span = soup.find_all('span', class_="major")
+	industry_span = soup.find_all('dd',class_="industry")
+	#pasdatad['Name'] = profile_name
+	titles = []
+	for row in title_span:
+		titles.append(list(row.stripped_strings))
+
+	#pasdatad[profile_name] = titles
+
+	for row in degree_span:
+		titles.append(list(row.stripped_strings))
+		#pasdatad['degree'] = degree
 	
-	with open("output.csv", "w") as f:
-		writer = csv.DictWriter(f, pasdatad.keys())
-		writer.writeheader()
-		writer.writerow(pasdatad)
+	
+	for row in industry_span:
+		titles.append(list(row.stripped_strings))
 
-if __name__ == '__main__':
-	main()
+	pasdatad[profile_name] = titles
+	with open('allData.json','w') as f:
+		json.dump(pasdatad,f,indent = 2)
+	    
 
-		# chmod 775 file.python makes executable
-		# add #!/usr/bin/python
-		# ls -l to check permissions
-		# read the google python syle editor
-		# add special function url for special cases
+    
+
+
+
+	#output.append(pasdatad)
